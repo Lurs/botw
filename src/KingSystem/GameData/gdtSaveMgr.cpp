d92ac43f23927febfa7ebcf9d96abd62c7469984
Lurs <l_werdes@gmx.de>
2024-09-27 15:48:25 +0200

    First 87% match of SaveMgr::init

--- src/KingSystem/GameData/gdtSaveMgr.cpp
+++ src/KingSystem/GameData/gdtSaveMgr.cpp
@@ -1,62 +1,116 @@
 #include "KingSystem/GameData/gdtSaveMgr.h"
 #include "KingSystem/Utils/HeapUtil.h"
-//#include "heap/seadExpHeap.h"
 #include "heap/seadHeap.h"
 #include "thread/seadDelegateThread.h"
 #include "thread/seadMessageQueue.h"
 #include "thread/seadThread.h"
+
 namespace ksys {
 
     SEAD_SINGLETON_DISPOSER_IMPL(SaveMgr)
 
     void SaveMgr::init(const InitArg& arg) {
-        mDoNotSave= arg.is_demo; 
-        sead::Heap *heap;               
-        mSaveAreaHeap = util::DualHeap::create(arg._8, sead::SafeString("SaveMgr"), arg.heap,nullptr,
-         sizeof(void*), sead::Heap::cHeapDirection_Forward, false);
-         
+       
+       mDoNotSave= arg.is_demo;
+        auto heap_size = (int32_t) arg._8;
+        mSaveAreaHeap = util::DualHeap::create(heap_size,sead::SafeString("SaveMgr"), arg.heap,arg._60,
+          sizeof(void*), sead::Heap::cHeapDirection_Forward, false);
+        u32 temp_68 = arg._68;
+        u64 buf_size;
+        u32 size;
+        u64 alignment;
+        void *buffer;
+        sead::Heap* heap = nullptr;
         sead::Heap* debug_heap2 = util::getDebugHeap2();
-        bool use_debug_heap = arg._68 != 0 && debug_heap2 != nullptr;
-        if (use_debug_heap) {
-            mSaveAreaHeap->tryAlloc(arg.buf_size, static_cast<s32>(arg.buf_alignment));
-            mSaveBufSize = arg.buf_size + arg._68;
+        bool use_Debug_Heap = debug_heap2 != nullptr && temp_68 != 0;
+        if (use_Debug_Heap) {
+            mSaveAreaHeap->tryAlloc(Casting(arg.buf_size), (s32) arg.buf_alignment);
+            //(sead::Buffer<sead::Heap*>) mSaveAreaHeap->tryAllocBuffer(arg.buf_size, mSaveAreaHeap, (s32) arg.buf_alignment);
+
+            size = arg.buf_size + temp_68;
+            mSaveBufSize = size;
+            alignment = arg.buf_alignment;
             heap = debug_heap2;
         }
         else {
             mSaveBufSize = arg.buf_size;
-            mSaveAreaHeap = debug_heap2;
+            size = arg.buf_size;           
             heap = mSaveAreaHeap;
+            alignment = arg.buf_alignment;
         }
-        
-        mBuf3 = operator new[](mSaveBufSize, mSaveAreaHeap, (s32) arg.buf_alignment);
-        std::memset(mBuf3, 0, mSaveBufSize);
 
-        mFieldE30 = operator new[](arg.size2, mSaveAreaHeap, (s32) arg.buf_alignment);
+        buffer = operator new[](size, heap, alignment);
+        buf_size = mSaveBufSize;
+        mSaveBuf = buffer;
+        //sead::MemUtil::fillZero(mSaveAreaHeap, mSaveBufSize);
+        std::memset(buffer, 0, buf_size);
+
+        mFieldE20 = operator new[](0x100, mSaveAreaHeap, 8);
+        //std::memset(mFieldE20, 0, 0x100);
+
+        mFieldE30 = operator new[](static_cast<int64_t>(static_cast<int32_t>(arg.size2)), mSaveAreaHeap, (s32) arg.buf_alignment);
+        
         std::memset(mFieldE30, 0, arg.size2);
         mFieldE38 = arg.size2;
 
-        sead::Delegate2<SaveMgr, sead::Thread*, sead::MessageQueue::Element> thread_params;
-        mThread= new sead::DelegateThread("SaveMgrThread",&thread_params, heap,
-                             (s32) arg.thread_priority,
-                             sead::MessageQueue::BlockType::Blocking, 0x7fffffff, (s32) arg._1c, 0x20);
+        //heap = mSaveAreaHeap;
+       
+        mThread= new sead::DelegateThread(
+            "SaveMgrThread", new (mSaveAreaHeap) sead::Delegate2 <SaveMgr, sead::Thread*,sead::MessageQueue::Element>(
+            this, &SaveMgr::SaveMgrThread),
+            mSaveAreaHeap,
+            (s32) arg.thread_priority,sead::MessageQueue::BlockType::Blocking,
+            0x7fffffff, (s32) arg._1c, 0x20);
         mThread->setAffinity(arg.thread_affinity);
         mThread->start();
         mountSaveData(arg);
-        this->mState = 0;
+        mState = 0;
 
         if (mDoNotSave) {
-            mIsRidDemo = *arg.save_rid_demo_dir.cstr() == sead::SafeStringBase<char>::cNullChar;
-            if (arg.save_rid_demo_dir.cstr()[0] == sead::SafeStringBase<char>::cNullChar && use_debug_heap) {
-                mSaveAreaHeap->tryAlloc(arg.buf_size, static_cast<s32>(arg.buf_alignment));
+            mIsRidDemo = arg.save_rid_demo_dir.isEmpty();
+            if (use_Debug_Heap) {
+                mSaveAreaHeap->tryAlloc(static_cast<int64_t>(static_cast<int32_t>(arg.buf_size)), (s32)arg.buf_alignment);
+                //buf_size = mSaveBufSize;
+                heap = debug_heap2;
             }
-            mBuf3 = operator new[](mSaveBufSize, mSaveAreaHeap, (s32) arg.buf_alignment);
-            std::memset(mBuf3, 0, mSaveBufSize);
+            else {
+                heap = mSaveAreaHeap;
+                //buf_size = mSaveBufSize;
+            }
+            buffer = operator new[](mSaveBufSize, heap, arg.buf_alignment);
+            mSaveBuf3 = buffer;
+            std::memset(buffer, 0,mSaveBufSize);
         }
         else {
             mIsRidDemo = false;
         }
-
-        _8 = 1;        
+    
     }
 
+    void SaveMgr::SaveMgrThread(sead::Thread* thread, sead::MessageQueue::Element element) {
+        switch(element) {
+            case 1:
+                 SaveMgrThreadCmd1();
+                 break;
+            case 2:
+                SaveMgrThreadCmd2();
+                break;
+            case 3:
+                SaveMgrThreadCmd3();
+                break;
+            case 4:
+                if(this->mSaveAreaHeap)
+                break;
+            default:
+                return;
+
+        }
+    }
+
+    u64 SaveMgr::Casting(u32 arg) const {
+        return static_cast<int64_t>(static_cast<int32_t>(arg));
+    }
+    
+
 }
+
--- src/KingSystem/GameData/gdtSaveMgr.h
+++ src/KingSystem/GameData/gdtSaveMgr.h
@@ -4,7 +4,10 @@
 #include <heap/seadDisposer.h>
 #include <mc/seadCoreInfo.h>
 #include <prim/seadSafeString.h>
-#include "KingSystem/Utils/Types.h"
+#include "KingSystem/GameData/gdtTriggerParam.h"
+#include "container/seadListImpl.h"
+#include "filedevice/seadArchiveFileDevice.h"
+#include "filedevice/seadFileDevice.h"
 #include "thread/seadThread.h"
 
 namespace ksys {
@@ -15,7 +18,7 @@ class SaveMgr {
     SaveMgr();
     virtual ~SaveMgr();
 public:
-    struct InitArg {
+     struct InitArg {
         sead::Heap* heap;
         u32 _8;
         u32 buf_size;
@@ -26,31 +29,148 @@ public:
         sead::CoreIdMask thread_affinity;
         sead::SafeString save_common_str = "save_common";
         sead::SafeString save_act_str = "save_act";
-        bool is_demo = false;
+        bool is_demo = 0;
         sead::SafeString save_rid_demo_dir;
-        void* _60 = nullptr;
+        sead::Heap* _60 = nullptr;
         u32 _68 = 0;
-        u32 _6c;
+        //u32 _6c = 0;
     };
-    KSYS_CHECK_SIZE_NX150(InitArg, 0x70);
-    bool mDoNotSave;
+    static_assert(sizeof(InitArg) == 0x70);
+    
     
 
     void init(const InitArg& arg);
     void mountSaveData(const InitArg& arg);
     void loadGameSaveData();
+    void SaveMgrThread(sead::Thread*, sead::MessageQueue::Element);
+    void SaveMgrThreadCmd1(); 
+    void SaveMgrThreadCmd2();
+    void SaveMgrThreadCmd3();
+
+    u64 Casting(u32 arg) const;
+
 private:
     sead::Heap* mSaveAreaHeap = nullptr;
-    u64 mSaveBufSize;
-    u32 _8;
-    sead::Thread* mThread;
+    sead::Thread* mThread = nullptr;
     u32 mState = 3;
-    void* mBuf3 = nullptr;
+    u32 _3c = 4;
+    u32 _40 = 4;
+    sead::FixedSafeString<0x20> _48;
+    sead::FixedSafeString<0x20> mSaveActStr2;
+    u16 _B8 = 0;
+    sead::FixedSafeString<0x20> mSaveActStr3;
+    //sead::FixedSafeString<0x20> _108;
+    u16 mFieldF8 = 1;
+    u16 _140 = 0;
+    u32 _144 = 0x1E;
+    u32 *mSaveSlotMaybe[0x84] = {0};
+    sead::FixedSafeString<0x100> _1D0;
+    u64 _318 = 0;
+    u64 _310 = 0;
+    u64 _308 = 0;
+    u64 _300 = 0;
+    u64 _2F8 = 0;
+    u64 _2F0 = 0;
+    u32 _320 = 0;
+    sead::FileHandle mFileHandle;
+    sead::ArchiveFileDevice mGameDataArc{nullptr};
+
+
+
+    
+    void* bgsvdata = nullptr;
+
+    
+    
+
+    sead::FixedSafeString<0x100> _E60;
+    u32 _F78 = 4;
+    u16 _F8C[8] = {0};
+    gdt::TriggerParam* mTriggerParam = nullptr;
+    u64 _F88 = 0xFFFFFFFF;
+    s32 _F90 = 0;
+    //vtable _FA09 = &off_71024DFF28
+    u32 _FB8 = 0;
+    struct _FA8 {
+        sead::ListNode* next;
+        sead::ListNode* prev;
+    };
+    //someCbList.size = 0;
+    //a1->someCbList.next = &a1->someCbList;
+    //a1->someCbList.prev = &a1->someCbList;
+    //vtable_FC0 = &off_71024DFF48;
+    //someCbList.next = &a1->someCbList;
+    //someCbList.prev = &a1->someCbList;
+    //vtable_FE0 = &off_71024DFF68;
+    //field_FF0 = &a1->field_FE8;
+    //field_FE8 = this->field_FE8;
+
+    //CallbackListfinishLoadCbs.size = 0;
+    u32 mCaptionPictSizeCrc32 = 0;
+    sead::DelegateFunc mInvoker3{}; //pInvoker3 = 0LL;
+    sead::DelegateFunc mInvoker8{}; //pInvoker8 = 0LL;
+    sead::DelegateFunc mInvokerAutoSave = {}; // invokerAutoSave = 0LL;
+    u32 mField_103C = 0xFFFFFFFF;
+    //vtable_1000 = &off_71024DFF88;
+    //finishLoadCbs.next = &a1->finishLoadCbs;
+    //finishLoadCbs.prev = &a1->finishLoadCbs;
+    u8 mCheatBuf[0x541]; 
+    
+    void* mSaveBuf = nullptr;   
+    u32 mSaveBufSize;
+    u8 mCheatBufx;
+    u32 _E18 = 0;    
+    void* mFieldE20 = nullptr;
+    u32 _E28 = 0;
+    u32 _E2C = 0;    
     void* mFieldE30 = nullptr;
     u32 mFieldE38 = 0;
-    u32 mFieldF8 = 0;
-    bool mIsRidDemo = false;
+    u32 mCheatBufy;    
+    s32 _E40 = 0;
+    s64 _E44 = 0;
+    u64 _E50 = 0;
+    s32 _E58 = 0;
+    //bool mCheatBuf3[2];
     
+    u8 mCheatBuf4[0x122];
+    void* mSaveBuf3 = nullptr;
+    u8 mCheatBuf5[0x70];
+    u32 mField_FF8 = 0;
+    u8 mCheatBuf6[0x40];
+    sead::SafeString mField_1040;
+    u64 mField_1050 = 0;   
+    bool mField_1058[1] = {0}; 
+    bool mField_1059 = true;
+    bool mIsRidDemo = true;
+
+    bool mDoNotSave = false;
+    //ksys::res::Handle::Handle(&this->res2b);
+    sead::FixedSafeString<0x100> mField_10B0;
+    u64 mField_11E0 = 0;
+    u64 mField_11D8 = 0;
+    //gap11C8.ptrs = 0LL;
+    //gap11C8.size = 0LL;
+    /*v3 = &a1->field_1208;
+    v4 = 0x7ELL;
+    a1->field_11D8 = &a1->field_11E8;
+    while ( 1 )
+    {
+    v3[0xFFFFFFFC] = v3 + 0xFFFFFFFE;
+    if ( !v4 )
+      break;
+    v3[0xFFFFFFFE] = v3;
+    v3 += 4;
+    v4 -= 2LL;
+  }
+  a1->field_19D8 = 0LL;
+  a1->field_11E0 = &a1->field_11E8;
+  sead::PtrArrayImpl::setBuffer(&a1->gap11C8, 0x80, a1->ptrArrayData); */
+        
+    
+    
+
+
 };
+
 
 }  // namespace ksys
